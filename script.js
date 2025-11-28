31316e57495d82bf048a1dff4fbffa421ee6c255
astikvaishnani-makdru <astik.vaishnani@gmail.com>
2025-11-28 20:31:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ function captureAndCompose() {
         const photoX = 25;
         const photoY = 320;
         //ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, photoX, photoY, photoWidth, photoHeight);
+        ctx.drawImage(video, photoX, photoY, photoWidth, photoHeight);
 
         // 2. Draw the PNG Frame OVER the photo
         ctx.drawImage(frameImg, 0, 0, POSTER_WIDTH, POSTER_HEIGHT);
@@ -140,6 +141,7 @@ resetBtn.addEventListener('click', resetApp);
 // Start the app on load
 
 window.onload = setupCamera;
+
 
 
 
